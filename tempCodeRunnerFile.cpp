fa6d8d1bdfc563e50989bf00ec298ba496c5afc4
akshay <ajvaghasiya69@gmail.com>
2023-08-09 15:07:34 +0530

    demo

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 1,n;
+    cin>>n;
+    for(i;i<=10;i++)
+    {
+    int result;
+    result = n * i ;
+
+    cout<<n<<"*"<<i<<"="<<result<<endl;
+    }
+}
